38fe187f162b5b6b7f705a668573d70dbc387331
Jimmy_A_Chiou <jac3rd>
2020-03-25 12:44:35 -0400

    edited markovdisks.cpp

--- markovdisks.cpp
+++ markovdisks.cpp
@@ -1,11 +1,22 @@
 #include "include.h"
+#include <cstdlib>
+#include <ctime>
+#include <cstdint>
+#include <forward_list>
 
 #define NUM_DISKS 64
+#define RADIUS 1
+#define DELTA 1
 #define BOX_WIDTH 10
 #define BOX_HEIGHT 10
-#define DELTA 1
 #define NUM_MOVES 100000
 
 int main() {
-    
+  std::srand(std::time(nullptr));
+  std::forward_list<Disk> actors;
+  for(uint8_t n = 0; n < NUM_DISKS; n++) {
+    double x = (BOW_WIDTH - 2 * RADIUS) * (std::rand()/(double)RAND_MAX - 0.5);
+    double y = (BOX_WIDTH - 2 * RADIUS) * (std::rand()/(double)RAND_MAX - 0.5);
+    actors.push_front(Disk(Vector(x, y)));
+  }
 }
